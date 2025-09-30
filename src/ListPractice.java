a90b4070ac2609cb42a68733826ea8f1ccfffdf1
Akai <161771111+Akaiokaa@users.noreply.github.com>
2025-09-29 19:56:37 -0700

    Completed half of ListPractice

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,23 +1,29 @@
+import java.util.ArrayList;
+
 public class ListPractice {
 
 
   public static void main(String[] args) {
     // Create an empty ArrayList of Strings and assign it to a variable of type List
-
+    ArrayList<String> list = new ArrayList<>();
     // Add 3 elements to the list (OK to do one-by-one)
-
+    list.add("Cake");
+    list.add("Ice Cream");
+    list.add("Macaron");
     // Print the element at index 1
-
+    System.out.println(list.get(0));
     // Replace the element at index 1 with a new value
     // (Do not insert a new value. The length of the list should not change)
-
+    list.set(1,"Bingsu");
+    System.out.println(list.get(1)+ " " + list.size());
     // Insert a new element at index 0 (the length of the list will change)
-
+    list.add(0,"Cookies");
+    System.out.println(list+ " " + list.size());
     // Check whether the list contains a certain string
-
+    System.out.println(list.contains("Cake") );
     // Iterate over the list using a traditional for-loop.
     // Print each index and value on a separate line
-
+    
     // Sort the list using the Collections library
 
     // Iterate over the list using a for-each loop
