66fb2c65aa43d0ef0e74be45bb501ef2f26fe414
Akai <161771111+Akaiokaa@users.noreply.github.com>
2025-10-01 13:33:14 -0700

    Slight adjustment to the first question

--- src/SetPractice.java
+++ src/SetPractice.java
@@ -1,9 +1,9 @@
 import java.util.HashSet;
-
+import java.util.Set;
 public class SetPractice {
   public static void main(String[] args) {
     // Create a HashSet of Strings and assign it to a variable of type Set
-    HashSet<String> hashbrown = new HashSet<>();
+    Set<String> hashbrown = new HashSet<>();
     // Add 3 elements to the set
     // (It's OK to do it one-by-one)
     hashbrown.add("potatoes");
